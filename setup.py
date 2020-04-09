7916b76227634f8f24f85350dbee24b0fb64f16a
mesmacosta <mesmacosta@gmail.com>
2020-04-09 13:26:43 -0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.rm') as history_file:
 
 setuptools.setup(
     name='datacatalog-util',
-    version='0.1.0',
+    version='0.1.1',
     author='Marcelo Miranda',
     author_email='mesmacosta@gmail.com',
     description='A package to manage Google Cloud Data Catalog'
--- src/datacatalog_util/__init__.py
+++ src/datacatalog_util/__init__.py
@@ -4,6 +4,6 @@ from .datacatalog_util_cli import main  # noqa: F401
 
 __author__ = """Marcelo Miranda"""
 __email__ = 'mesmacosta@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ('main')
